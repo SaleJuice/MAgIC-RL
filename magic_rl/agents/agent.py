1058fde22e81a0d07d02f2c678d7b57998c179b5
linxzh <linxzh@shanghaitech.edu.cn>
2023-01-23 19:18:26 +0800

    create a abstact class 'Agent' for all different rl agent.

---
+++ magic_rl/agents/agent.py
@@ -0,0 +1,28 @@
+'''
+FilePath: /MAgIC-RL/magic_rl/agents/agent.py
+Date: 2022-12-18 11:30:59
+LastEditTime: 2022-12-18 11:39:53
+Author: Xiaozhu Lin
+E-Mail: linxzh@shanghaitech.edu.cn
+Institution: MAgIC Lab, ShanghaiTech University, China
+SoftWare: VSCode
+'''
+
+
+class Agent(object):
+    '''
+    This is a base class for all rl agents.
+    '''
+    
+    def __init__(self) -> None:
+        pass
+
+    def get_action(self):
+        pass
+
+    def update(self):
+        pass
+
+    def save_model(self):
+        pass
+
